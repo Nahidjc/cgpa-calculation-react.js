12db4d949868609d0eeb404e02516860b35ae1f4
Nahid2194 <nahidjc80@gmail.com>
2021-05-10 20:08:01 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Course from './components/Course';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    courses: [
+      { credit: 3, grade: 4.0 },
+
+    ]
+  }
+  handleChange = (i, change) => {
+    const courses = this.state.courses.slice();
+    courses[i] = { ...courses[i], ...change };
+    this.setState({ courses: courses });
+  }
+  addCourse() {
+    this.setState({
+      courses: [...this.state.courses, { credit: 3, grade: 4.0 }]
+    })
+  }
+  render() {
+    let totalCredits = 0, totalGrade = 0;
+    const courses = this.state.courses.map((course, i) => {
+      totalCredits += course.credit;
+      totalGrade += course.grade;
+      return <Course index={i} handleChange={this.handleChange.bind(this)} />;
+    })
+    return (
+      <div className="container">
+        {courses}
+        <button className="button is-primary is-rounded" onClick={this.addCourse.bind(this)}>+</button>
+        <div className="control is-centered">
+          <div className="gpa tags has-addons">
+            <div className="tag is-dark">
+              <div className="tag is-info">
+                <p>{(totalGrade / totalCredits).toFixed(2)}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Course.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class Course extends Component {
+    handleGradeChange = event => {
+        this.props.handleChange(this.props.index, { grade: Number(event.target.value) });
+    }
+    handleCreditsChange = event => {
+        this.props.handleChange(this.props.index, { credit: Number(event.target.value) });
+    }
+    render() {
+        return (
+            <div className="field is-grouped is-grouped-centered">
+                <div className="control">
+                    <div className="select">
+                        <select onChange={this.handleCreditsChange.bind(this)}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+
+                        </select>
+                    </div>
+                </div>
+                <div className="control">
+                    <div className="select">
+                        <select onChange={this.handleGradeChange.bind(this)}>
+                            <option value="4" selected>A+</option>
+                            <option value="3.75">A</option>
+                            <option value="3.5">A-</option>
+                            <option value="3.25">B+</option>
+                            <option value="3">B</option>
+                            <option value="2.75">B-</option>
+                            <option value="2.5">C+</option>
+                            <option value="2.25">C</option>
+                            <option value="2">D</option>
+                        </select>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Course;
